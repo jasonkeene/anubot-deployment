7cf2f1f78710aa91ad6da4513fbc78c348b96211
Jason Keene <jasonkeene@gmail.com>
2016-11-14 19:57:50 -0700

    Add cloud config to run api-server, closes jasonkeene/anubot#60

--- alpha.tf
+++ alpha.tf
@@ -4,11 +4,25 @@ resource "digitalocean_ssh_key" "anubot" {
     public_key = "${var.anubot_ssh_public_key}"
 }
 
+data "template_file" "api_server_user_data" {
+    template = "${file("user_data/api_server")}"
+    vars {
+        tls_key = "${replace(file("certs/anubot.io.key"), "\n", "\n    ")}"
+        tls_cert = "${replace(file("certs/anubot.io.combined"), "\n", "\n    ")}"
+        discord_client_id = "${var.anubot_discord_oauth_client_id}"
+        discord_client_secret = "${var.anubot_discord_oauth_client_secret}"
+        discord_redirect_uri = "${var.anubot_discord_oauth_redirect_uri}"
+        twitch_client_id = "${var.anubot_twitch_oauth_client_id}"
+        twitch_client_secret = "${var.anubot_twitch_oauth_client_secret}"
+        twitch_redirect_uri = "${var.anubot_twitch_oauth_redirect_uri}"
+    }
+}
+
 resource "digitalocean_droplet" "api_server" {
     image = "coreos-stable"
     name = "api-server"
     region = "nyc3"
     size = "512mb"
     ssh_keys = ["${digitalocean_ssh_key.anubot.id}"]
-    user_data = "${file("user_data/api_server")}"
+    user_data = "${data.template_file.api_server_user_data.rendered}"
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,29 @@
 variable "anubot_ssh_public_key" {
     type = "string"
 }
+
+variable "anubot_discord_oauth_client_id" {
+    type = "string"
+}
+
+variable "anubot_discord_oauth_client_secret" {
+    type = "string"
+}
+
+variable "anubot_discord_oauth_redirect_uri" {
+    type = "string"
+    default = "https://anubot.io/discord_oauth/done"
+}
+
+variable "anubot_twitch_oauth_client_id" {
+    type = "string"
+}
+
+variable "anubot_twitch_oauth_client_secret" {
+    type = "string"
+}
+
+variable "anubot_twitch_oauth_redirect_uri" {
+    type = "string"
+    default = "https://anubot.io/twitch_oauth/done"
+}
