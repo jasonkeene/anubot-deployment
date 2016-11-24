7703b2954f1ed6cc7f874baae4a1fbc51bb579ab
Jason Keene <jasonkeene@gmail.com>
2016-11-24 13:37:16 -0700

    Add floating IP to DNS vs dropplet IP, jasonkeene/anubot#58

--- dns.tf
+++ dns.tf
@@ -1,5 +1,10 @@
 
+resource "digitalocean_floating_ip" "api_server" {
+    droplet_id = "${digitalocean_droplet.api_server.id}"
+    region = "nyc1"
+}
+
 resource "digitalocean_domain" "anubot" {
     name = "anubot.io"
-    ip_address = "${digitalocean_droplet.api_server.ipv4_address}"
+    ip_address = "${digitalocean_floating_ip.api_server.ip_address}"
 }
